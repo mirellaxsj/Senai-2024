4bb4e2b04bfe56311caa20cba000037f9c53c8c9
mirellaxsj <mirellafrancasenai@gmail.com>
2024-05-16 08:40:09 -0300

    autogit

--- 01-pwbe/aula11/exemplo_prisma/src/routes.js
+++ 01-pwbe/aula11/exemplo_prisma/src/routes.js
@@ -14,6 +14,9 @@ router.get('/clientes', clientes.read);
 router.delete('/clientes/:id', clientes.remove);
 router.put('/clientes/:id', clientes.update);
 
+router.get('/clientes/:id', clientes.readById);
+router.post('/clientes/:nome', clientes.readByName);
+
 //Telefones 
 router.post('/telefones', telefones.create);
 router.get('/telefones', telefones.read);
